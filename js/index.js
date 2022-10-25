901f376d76c8f409467b95de21d9d8c725d4c631
Jacob <dokygamr@outlook.com>
2022-10-24 19:33:51 -0700

    Updated theme

--- js/index.js
+++ js/index.js
@@ -1,23 +1,15 @@
 // Animate on scroll
 
-window.addEventListener("scroll",animate)
-window.addEventListener("load",animate)
-
-function animate(){
-    let elements = document.querySelectorAll("body *")
-    let windowHeight = window.innerHeight
-    for(let element of elements){
-        let elementYPosition = element.getBoundingClientRect().top
-        if(elementYPosition + 150 < windowHeight){
-            element.classList.add("appeared")
-            
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
         }
-    }
-}
+    });
+});
 
-// Toggle light/dark mode
-
-function toggleDark() {
-    var element = document.body;
-    element.classList.toggle("light-mode");
-}
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
